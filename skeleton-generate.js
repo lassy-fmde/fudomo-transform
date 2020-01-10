5381a84d6d7e7909e60a3fab3f4fbf0510f5a152
Christian Glodt <chris@mind.lu>
2020-01-10 10:50:49 +0100

    separate skeleton generation into a method for decompositions that is called by an abstract base method for a whole transformation.

--- skeleton-generate.js
+++ skeleton-generate.js
@@ -67,81 +67,103 @@ class CommentFormatter {
   }
 }
 
-class JSSkeletonGenerator {
+class SkeletonGenerator {
+  constructor(prefix, separator, suffix) {
+    this.prefix = prefix;
+    this.separator = separator;
+    this.suffix = suffix;
+  }
+
   generateSkeleton(transformation) {
+    let res = this.prefix;
 
-    let res = 'module.exports = {\n';
+    const decompositionSkeletons = transformation.decompositions.map(this.generateDecompositionFunction);
+    res += decompositionSkeletons.join(this.separator);
 
-    for (var decomposition of transformation.decompositions) {
-      const fmt = new CommentFormatter('  /**\n', '\n   */', '   * ', 79);
+    res += this.suffix;
+    return res;
+  }
 
-      const funcName = decomposition.function.type + '_' + decomposition.function.name;
+  generateDecompositionFunction(decomposition) {
+    throw new Error('Not implemented');
+  }
+}
 
-      var params = [];
-      for (var link of decomposition.links) {
-        params.push(link.parameterName);
-      }
+class JSSkeletonGenerator extends SkeletonGenerator {
+  constructor() {
+    super('module.exports = {\n', '\n\n', '\n};\n');
+  }
 
-      fmt.wrapText(decomposition.function.qualifiedName);
-      if (decomposition.comment) {
-        fmt.wrapText(decomposition.comment);
-      }
+  generateDecompositionFunction(decomposition) {
 
-      const links = decomposition.links;
-      for (const index of Object.keys(links)) {
-        const link = links[index];
-        const type = link.parameterTypeDescription;
-        if (type) {
-          fmt.wrapTextWithPrefix(`@param ${link.parameterName} {${type}} `, link.parameterDescription);
-        } else {
-          fmt.wrapTextWithPrefix(`@param ${link.parameterName} `, link.parameterDescription);
-        }
-      }
+    const fmt = new CommentFormatter('  /**\n', '\n   */', '   * ', 79);
 
-      res += fmt.toString();
-      res += '\n';
+    const funcName = decomposition.function.type + '_' + decomposition.function.name;
 
-      const paramsStr = params.join(', ');
-      res += `  ${funcName}: function(${paramsStr}) {\n`;
-      res += `    throw new Error('function ${funcName}(${paramsStr}) not yet implemented'); // TODO\n`;
-      res += '  },\n\n';
+    var params = [];
+    for (var link of decomposition.links) {
+      params.push(link.parameterName);
     }
 
-    res += '};\n';
+    fmt.wrapText(decomposition.function.qualifiedName);
+    if (decomposition.comment) {
+      fmt.wrapText(decomposition.comment);
+    }
+
+    const links = decomposition.links;
+    for (const index of Object.keys(links)) {
+      const link = links[index];
+      const type = link.parameterTypeDescription;
+      if (type) {
+        fmt.wrapTextWithPrefix(`@param ${link.parameterName} {${type}} `, link.parameterDescription);
+      } else {
+        fmt.wrapTextWithPrefix(`@param ${link.parameterName} `, link.parameterDescription);
+      }
+    }
+
+    let res = fmt.toString();
+    res += '\n';
+
+    const paramsStr = params.join(', ');
+    res += `  ${funcName}: function(${paramsStr}) {\n`;
+    res += `    throw new Error('function ${funcName}(${paramsStr}) not yet implemented'); // TODO\n`;
+    res += '  },';
+
     return res;
   }
 }
 
-class PythonSkeletonGenerator {
-  generateSkeleton(transformation) {
-    let res = '';
+class PythonSkeletonGenerator extends SkeletonGenerator {
+  constructor() {
+    super('', '\n\n\n', '');
+  }
 
-    for (var decomposition of transformation.decompositions) {
-      const fmt = new CommentFormatter('    """', '\n    """\n', '    ', 79);
+  generateDecompositionFunction(decomposition) {
 
-      const funcName = decomposition.function.type + '_' + decomposition.function.name;
+    const fmt = new CommentFormatter('    """', '\n    """\n', '    ', 79);
 
-      var params = decomposition.links.map(link => link.parameterName);
+    const funcName = decomposition.function.type + '_' + decomposition.function.name;
 
-      res += `def ${funcName}(${params.join(', ')}):\n`;
+    var params = decomposition.links.map(link => link.parameterName);
 
-      fmt.wrapText(decomposition.function.qualifiedName);
+    let res = `def ${funcName}(${params.join(', ')}):\n`;
 
-      if (decomposition.comment) {
-        fmt.wrapText(decomposition.comment);
-      }
+    fmt.wrapText(decomposition.function.qualifiedName);
 
-      for (var link of decomposition.links) {
-        fmt.wrapTextWithPrefix(`:param ${link.parameterName}: `, `${link.parameterDescription}`)
-        const type = link.parameterTypeDescription;
-        if (type) {
-          fmt.wrapTextWithPrefix(`:type  ${link.parameterName}: `, `${type}`);
-        }
-      }
-
-      res += fmt.toString()
-      res += `    raise NotImplementedError('function ${funcName} not yet implemented')  # TODO\n\n\n`;
+    if (decomposition.comment) {
+      fmt.wrapText(decomposition.comment);
     }
+
+    for (var link of decomposition.links) {
+      fmt.wrapTextWithPrefix(`:param ${link.parameterName}: `, `${link.parameterDescription}`)
+      const type = link.parameterTypeDescription;
+      if (type) {
+        fmt.wrapTextWithPrefix(`:type  ${link.parameterName}: `, `${type}`);
+      }
+    }
+
+    res += fmt.toString()
+    res += `    raise NotImplementedError('function ${funcName} not yet implemented')  # TODO`;
 
     return res.trim();
   }
